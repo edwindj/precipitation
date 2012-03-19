38ca7d5d9c935bc0505edb5d7e6956be2f9c161e
Edwin de Jonge <edwindjonge@gmail.com>
2012-03-19 09:38:35 +0100

    fixed coordinate problems

--- precipitation.R
+++ precipitation.R
@@ -33,7 +33,7 @@ if (DOWNLOADDATA=="y"){
 }
 
 # create a reaster
-r <- raster(nrows=72, ncols=144, xmn=-180, xmx=180, ymn=-90, ymx=90)
+r <- raster(nrows=72, ncols=144, xmn=0, xmx=360, ymn=-90, ymx=90)
 
 dir.create("data/africa",recursive=TRUE)
 files.africa <- paste("data/africa/af_", m,".grd", sep="")
@@ -43,14 +43,17 @@ ext.africa <- extent(c(-25, 60, -45,40))
 for (i in seq_along(files)){
   tab <- read.table(gzfile(files[i]))
   values(r) <- as.matrix(tab)
-  r.africa <- crop(r, ext.africa)
+  # rotate data and restrict to africa
+  r.africa <- crop(rotate(r),ext.africa)
   r.africa <- disaggregate(r.africa, fact=5)
   writeRaster(r.africa, filename=files.africa[i], overwrite=TRUE)
   cat("Writing ", files.africa[i], "...[",i,"/",length(files),"]\n")
 }
 
 africa <- brick(stack(files.africa), filename="data/africa.grd", overwrite=TRUE)
-africa.u <- mapply(unstack(africa),frac, FUN=`*`)
+# total rain per month
+africa.u <- mapply(unstack(africa),days, FUN=`*`)
+
 africa.m <- brick(stack(africa.u), filename="data/africa.m.grd", overwrite=TRUE)
 
 # data per grid cell per year
@@ -59,10 +62,10 @@ africa_year <- stackApply( africa.m, indices=col(m), filename="data/africa_year.
                          )
 layerNames(africa_year) <- paste("Y", years, sep="")
 library(maptools)
+gpclibPermit()
 country <- readShapePoly("TM_WORLD_BORDERS-0.3/TM_WORLD_BORDERS-0.3.shp")
 rcountry <- rasterize(country, africa_year)
 
-# TODO extract year data per country
 cnts <- unique(values(rcountry))[-1]
 names(cnts) <- country$NAME[cnts]
 cntsCells <- t(sapply( cnts
